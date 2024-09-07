ac2da4d5249b43e94dcd5cecba4275a72e38545d
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-09-07 12:55:36 +0200

    chore: Update self-hosted component styling in root layout

--- components/selfhosted.tsx
+++ components/selfhosted.tsx
@@ -1,10 +1,6 @@
-export const SelfHosted = () => {
-  const isSelfhosted = process.env.ISSELFHOSTED === "true";
-  if (!isSelfhosted) return null;
-
-  return (
-    <div className="pointer-events-none fixed -right-20 top-[3.7rem] z-50 origin-top-left rotate-90 transform text-center text-xs text-primary">
+export const SelfHosted = () =>
+  process.env.ISSELFHOSTED === "true" && (
+    <div className="pointer-events-none fixed -right-[4.2rem] top-[2.8rem] z-50 origin-bottom-left rotate-90 transform text-center text-xs text-primary">
       proudly self-hosted
     </div>
   );
-};
